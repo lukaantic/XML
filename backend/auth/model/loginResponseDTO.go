9a64bdeaa381b1795bdd1f00ca650141b15c0188
lukaantic <lukalazy@gmail.com>
2022-08-22 18:56:34 +0200

    Revert "authentication update"
    This reverts commit 476b67f98ee10c8fc13d8250c0793ca10360c4df.

--- backend/auth/model/loginResponseDTO.go
+++
@@ -1,5 +0,0 @@
-package model
-
-type LoginResponse struct {
-	Token string `json:"token"`
-}
--- backend/auth/model/userRole.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type UserRole int
-
-const(
-	Regular UserRole = iota
-	Administrator
-	Agent
-)
