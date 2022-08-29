aa13a2b989679eb8a82d46c9397a292a0372f4ff
lukaantic <62259635+lukaantic@users.noreply.github.com>
2022-08-29 23:02:33 +0200

    Some random commit to check witch account is commiting

--- FrontendApp/dislinkt/src/pages/home/Home.js
+++ FrontendApp/dislinkt/src/pages/home/Home.js
@@ -39,7 +39,7 @@ export default function RecipeReviewCard() {
             <CardHeader
                 avatar={
                 <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                    R
+                    Dusan
                 </Avatar>
                 }
                 action={
