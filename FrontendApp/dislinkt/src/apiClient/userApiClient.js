b047014232d49b35e08ae267aaf8d067d3c1eb28
lukaantic <62259635+lukaantic@users.noreply.github.com>
2022-08-29 19:55:00 +0200

    Initial commit

---
+++ FrontendApp/dislinkt/src/apiClient/userApiClient.js
@@ -0,0 +1,11 @@
+import { axiosInstance } from '../axios/axios';
+
+// export const login = async payload => {
+//     const data = await axiosInstance.post('/user/login', payload);
+//     return data;
+// };
+
+// export const signUp = async payload => {
+//     const data = await axiosInstance.post('/user/signup', payload);
+//     return data;
+// };
